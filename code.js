8c2dad6d33ce0a4d4913c2da218e9950809406e9
scruffy21 <yougotmebeggingyouformercy@gmail.com>
2018-03-15 12:15:07 +0100

    polyfill includes

--- code.js
+++ code.js
@@ -252,17 +252,31 @@ if (!Array.prototype.diff) {
 }
   
 //polyfill for IE
-if (!String.prototype.includes) {
-    String.prototype.includes = function(search, start) {
-      'use strict';
-      if (typeof start !== 'number') {
-        start = 0;
-      }
+// https://tc39.github.io/ecma262/#sec-array.prototype.includes
+if (!Array.prototype.includes) {
+    Object.defineProperty(Array.prototype, 'includes', {
+      value: function(searchElement, fromIndex) {
   
-      if (start + search.length > this.length) {
+        if (this == null) {
+          throw new TypeError('"this" is null or not defined');
+        }
+        var o = Object(this);
+        var len = o.length >>> 0;
+        if (len === 0) {
+          return false;
+        }
+        var n = fromIndex | 0;
+        var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+        function sameValueZero(x, y) {
+          return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+        }
+        while (k < len) {
+          if (sameValueZero(o[k], searchElement)) {
+            return true;
+          }
+          k++;
+        }
         return false;
-      } else {
-        return this.indexOf(search, start) !== -1;
       }
-    };
-  }
+    });
+}
